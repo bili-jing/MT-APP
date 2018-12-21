31962cb2e49fe998ceb0a2a6ffe2651b05443dfe
LiYan <bili.jing.liyan@gmail.com>
2018-12-22 01:08:36 +0800

    切换城市完成

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import Koa from "koa";
 const consola = require("consola");
 const { Nuxt, Builder } = require("nuxt");
 
+
 import mongoose from "mongoose";
 import bodyParser from "koa-bodyparser";
 import session from "koa-generic-session";
